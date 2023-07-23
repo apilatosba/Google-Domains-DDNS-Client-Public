778403991262a8a111b80d69fa0a95cdf41a1222
apilatosba <aposdapulos098@outlook.com>
2023-07-23 21:03:31 +0300

    no warnings. 1984

--- Google Domains DDNS Client/Credentials.cs
+++ Google Domains DDNS Client/Credentials.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Serialization;
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+using System.Xml.Serialization;
 
 namespace Google_Domains_DDNS_Client {
    public class Credentials {
